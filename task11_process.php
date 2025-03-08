c5545cd2e3c9fef105a8e019bedf71f3b1b33138
kanweb <63344958+kanweb@users.noreply.github.com>
2025-03-08 23:23:43 +0300

    Задание 11

---
+++ task11_process.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+if (!isset($_POST["try"])) {
+    $_SESSION['error'] = 'Введите число';
+    header('Location: task11.php');
+    exit;
+}
+
+if ($_POST["try"] < 0 || $_POST["try"] > 20) {
+    $_SESSION['error'] = 'Ошибка ввода, неверный диапазон';
+    header('Location: task11.php');
+    exit;
+}
+
+
+if ($_POST["try"] > $_SESSION['hidden']) {
+    $_SESSION["attempts"]--;
+    $_SESSION['error'] = 'Загаданное число меньше.';
+} elseif ($_POST["try"] < $_SESSION['hidden']) {
+    $_SESSION["attempts"]--;
+    $_SESSION['error'] = 'Загаданное число больше.' ;
+} elseif ($_POST["try"] == $_SESSION['hidden']) {
+    $_SESSION['success'] = 'Вы угадали! Загаданное число '.$_SESSION['hidden'].'. Сыграем еще разок!';
+}
+
+if ( $_SESSION['attempts']==0) {
+    $_SESSION['error'] = 'Попытки закончились. Загаданное число '.$_SESSION['hidden'].'.';
+}
+
+
+header('Location: task11.php');
+exit; // Завершаем скрипт
+
+
+?>
