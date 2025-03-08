7cb81e017d83fc1d4980f0fc53dfbbaf9d9432df
kanweb <63344958+kanweb@users.noreply.github.com>
2025-03-08 23:29:22 +0300

    Задание 11
    Добавлена проверка на количество попыток, Неудачных

--- task11_process.php
+++ task11_process.php
@@ -13,7 +13,7 @@ if ($_POST["try"] < 0 || $_POST["try"] > 20) {
     exit;
 }
 
-//Проверяем меньше /больше / равно
+//Проверяем меньше / больше / равно
 if ($_POST["try"] > $_SESSION['hidden']) {
     $_SESSION["attempts"]--;
     $_SESSION['error'] = 'Загаданное число меньше.';
@@ -28,7 +28,6 @@ if ($_POST["try"] > $_SESSION['hidden']) {
 if ($_SESSION['attempts'] == 0 && isset($_SESSION['error'])) {
     $_SESSION['error'] = 'Попытки закончились. Загаданное число ' . $_SESSION['hidden'] . '.';
 }
-
 
 header('Location: task11.php');
 exit; // Завершаем скрипт
